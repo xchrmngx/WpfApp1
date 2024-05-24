9a6c6e97995b8b86b6af9784a73b0e5487018a72
xchrmngx <nvidio960@mail.ru>
2024-05-24 23:29:48 +0500

    Добавьте файлы проекта.

---
+++ Logpanel.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Logpanel.xaml
+    /// </summary>
+    public partial class Logpanel : Window
+    {
+        public Logpanel()
+        {
+            InitializeComponent();
+        }
+        trenyaEntities5 db = new trenyaEntities5();
+        List<users> regusers = new List<users>();
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var user = db.users.FirstOrDefault(x => x.login == textbox1.Text && x.password == textbox2.Text);
+
+            if (user == null) 
+            {
+                MessageBox.Show("Данные неверны");
+            }
+            else if (user.password == textbox2.Text && user.login == textbox1.Text)
+            {
+                if (user.type == 1)
+                {
+                    User1 okno = new User1(user);
+                    okno.Show();
+                    Close();
+                }
+                else if (user.type == 2) 
+                {
+                    User2 okno2 = new User2(user);
+                    okno2.Show();
+                    Close();
+                }
+                else if (user.type == 3)
+                {
+                    User3 okno3 = new User3(user);
+                    okno3.Show();
+                    Close();
+                }
+            }
+        }
+
+
+        private void button_reg_Click(object sender, RoutedEventArgs e)
+        {
+            int a = Convert.ToInt32(type_box.Text);
+            var user = db.users.FirstOrDefault(x => x.login == login_box.Text && x.password == pass_box.Text && x.FIO == fio_box.Text && x.type == a);
+            if (login_box.Text != "" && pass_box.Text != "" && fio_box.Text != "")
+            {
+                users users = new users
+                {
+                    login = login_box.Text,
+                    password = pass_box.Text,
+                    FIO = fio_box.Text,
+                    type = a
+                };
+                db.users.Add(users);
+                regusers.Add(user);
+                db.SaveChanges();
+                MessageBox.Show("Пользователь добавлен");
+            }
+            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        trenyaEntities5 db = new trenyaEntities5();
+        
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+                var data = db.fio.FirstOrDefault();
+                if (data != null)
+                {
+                    string value = data.фио;
+                    textbox.Text = value;
+                    label.Content = value;
+                }
+            var data2 = db.fio.ToList();
+            var comboBoxItems = data2.Select(x => new ComboBoxItem { Content = x.фио});
+            combobox.ItemsSource = comboBoxItems;
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+            var data = db.fio.FirstOrDefault(x => x.фио == proverka.Text);
+
+            if (data == null)
+            {
+                MessageBox.Show("f"); // Данные не найдены
+            }
+            else
+            {
+                if (data.фио == proverka.Text)
+                {
+                    MessageBox.Show("a"); // Данные совпадают
+                }
+            }
+
+        }
+    }
+}
